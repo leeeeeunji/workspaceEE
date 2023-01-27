30ee9f29b1387be2e2dec7afde2c53ba67bf9da3
leeeeeunji <qwas2555@naver.com>
2023-01-27 18:09:42 +0900

    ..

---
+++ guest_model1/src/com/itwill/guest/GuestService.java
@@ -0,0 +1,17 @@
+package com.itwill.guest;
+
+import java.util.List;
+
+public class GuestService {
+	private GuestDao guestDao;
+	
+	public GuestService() throws Exception{
+		guestDao = new GuestDao();
+	}
+	/*
+	 * 방명록 리스트
+	 */
+	public List<Guest> findAll() throws Exception{
+		return guestDao.findAll();
+	}
+}
---
+++ guest_model1/src/com/itwill/guest/GuestServiceServiceTestMain.java
@@ -0,0 +1,11 @@
+package com.itwill.guest;
+
+public class GuestServiceServiceTestMain {
+
+	public static void main(String[] args) throws Exception {
+		GuestService guestService = new GuestService();
+		System.out.println(guestService.findAll());
+
+	}
+
+}
